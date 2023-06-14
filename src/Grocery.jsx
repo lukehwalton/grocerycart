8ecdbf4092ac983dd3cb1e3f0b133680056afb24
Luke H. Walton <lukehwalton@icloud.com>
2023-06-14 08:33:11 -0700

    done with simple grocery button, have begun grocery list

--- src/Grocery.jsx
+++ src/Grocery.jsx
@@ -1,3 +1,37 @@
-const Grocery = ({name, price, quantity, img}) => {
-  
+import {useState} from 'react';
+
+const Grocery = ({name, price, quantity, img, type, addRemoveImg}) => {
+  const [disabled, setDisabled] = useState(false);
+  const [curQuantity, setQuantity] = useState(quantity);
+  const handleClick = () =>{
+    setQuantity(curQuantity - 1);
+  }
+
+  const buttonStyle={
+    width: '30ch',
+    marginBottom: '15px',
+    height: '70px',
+    padding: '10px'
+  }
+
+  const imgStyle={
+    width:'50px',
+    height:'50px'
+  }
+  const imgSrc = `${img}`
+  const addRemoveSrc = `../assets/${addRemoveImg}`
+
+  return(
+    <div>
+      <button
+        className={`btn btn-outline-${type}`}
+        style={buttonStyle}
+        onClick={handleClick}>
+          <img src={img} alt='' style={imgStyle}/>
+        {name} - {price} ({curQuantity})
+      </button>
+    </div>
+  )
 }
+
+export default Grocery;
